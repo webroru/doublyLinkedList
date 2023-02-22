ad1aa146967d1f61cc9200b9b3e5506f9184e55b
Igor Nemykin <i.nemykin@devcaz.com>
2023-02-22 16:43:29 +0100

    add Node class

--- Node.php
+++ Node.php
@@ -1,8 +1,13 @@
 <?php
 
-namespace App;
+declare(strict_types=1);
 
 class Node
 {
+    public ?Node $next = null;
+    public ?Node $prev = null;
 
+    public function __construct(public mixed $data)
+    {
+    }
 }
